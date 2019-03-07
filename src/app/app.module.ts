7a894f9d8c5c110c7ae747429dd6ea5c05068c59
Your Name <you@example.com>
2019-03-07 11:54:36 +0100

    appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -33,6 +33,6 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // EXECUTER EN PREMIER
 })
 export class AppModule { }
